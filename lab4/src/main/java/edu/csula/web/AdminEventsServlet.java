bc6a5a54ab7d76748ca6a75fb1e92a4113e5af6a
Nicolas Sirimongkol <nsirimo@calstatela.edu>
2018-04-13 09:48:06 -0700

    Add doGet servlet for getting html and doPost for handling deletion and adding events, will have to change this to doGet soon

--- lab4/src/main/java/edu/csula/web/AdminEventsServlet.java
+++ lab4/src/main/java/edu/csula/web/AdminEventsServlet.java
@@ -16,20 +16,109 @@ import edu.csula.models.Event;
 
 @WebServlet("/admin/events")
 public class AdminEventsServlet extends HttpServlet {
+	private static final String ADD = "add";
+	private static final String DELETE = "delete";
+
 	@Override
-	public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		// TODO: render the events page HTML
 		EventsDAO dao = new EventsDAOImpl(getServletContext());
 		Collection<Event> events = dao.getAll();
 		System.out.println(events);
-		out.println("<h1>Hello events servlet!</h1>");
+		out.println("<html>");
+		out.println("	<head>");
+		out.println("		<meta charset=\"UTF-8\">");
+		out.println("		<title>Doge Coin Game</title>");
+		out.println("		<link rel=\"stylesheet\" type=\"text/css\" href=\"../custom.css\">");
+		out.println("	</head>");
+		out.println("	<body>");
+		out.println("		<div class=\"main\">");
+		out.println("			<h1 class=\"pageTitle\">Dat Doge Coin Gen!</h1>");
+		out.println("			<nav class=\"navBar\">");
+		out.println("				<a href=\"admin-info.html\">Game Information</a>");
+		out.println("				<a href=\"admin-generators.html\">Coin Generators</a>");
+		out.println("				<a href=\"admin-events.html\">");
+		out.println("    				<div class=\"navTitle\">Events</div>");
+		out.println("				</a>");
+		out.println("			</nav>");
+		out.println("			</div>");
+		out.println("			<div class = \"container\">");
+		out.println("				<div class = \"genForm\">");
+		out.println("					<form method = \"POST\">");
+		out.println("						<label for = \"event_name\">Event Name:</label>");
+		out.println(
+				"							<input type=\"text\" id = \"event_name\" name = \"event_name\" value=\"\" required>");
+		out.println("						<label for = \"description\">Event Description</label>");
+		out.println(
+				"							<textarea id = \"description\" name=\"description\" value=\"\" required></textarea>");
+		out.println("						<label for = \"trigger\">Trigger at</label>");
+		out.println(
+				"							<input type=\"number\" id=\"trigger\" name=\"trigger\" value=\"\" required>");
+		out.println(
+				"							<input type=\"submit\" name=\"status\" value=\"add\">{Add|Edit}</input>");
+		out.println("					</form>");
+		out.println("				</div>");
+		out.println("				<table>");
+		out.println("					<tr>");
+		out.println("						<th>Name</th>");
+		out.println("						<th>Description</th>");
+		out.println("						<th>Trigger At</th>");
+		out.println("					</tr>");
+		for (Event event : events) {
+			out.println("					<form method = \"POST\">");
+			out.println("						<tr>");
+			out.println("						<td>" + event.getName() + "</td>");
+			out.println("						<td>" + event.getDescription() + "</td>");
+			out.println("						<td>" + event.getTriggerAt() + "</td>");
+			out.println(
+					"						<td> <input type=\"submit\" name=\"status\" value=\"delete\"></input> <button> Edit </button>");
+			out.println("						</tr>");
+			out.println("					</form>");
+		}
+		out.println("					</table>");
+		out.println("				</div>");
+		out.println("			</div>");
+		out.println("		</div>");
+		out.println("	</body>");
+		out.println("</html>");
 	}
-
 
 	@Override
-	public void doPost( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO: handle upsert transaction
+
+		System.out.println("this: " + request.getParameter("status"));
+		String action = request.getParameter("status");
+		if (ADD.equals(action)) {
+			add(request, response);
+		} else if (DELETE.equals(action)) {
+			delete(request, response);
+		}
 	}
+
+	private void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("add");
+		EventsDAO dao = new EventsDAOImpl(getServletContext());
+		Collection<Event> events = dao.getAll();
+		String event_name = request.getParameter("event_name");
+		String description = request.getParameter("description");
+		int trigger = Integer.parseInt(request.getParameter("trigger"));
+
+		Event event = new Event(events.size(), event_name, description, trigger);
+		dao.add(event);
+
+		response.sendRedirect("/admin/events");
+	}
+
+	private void delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Delete");
+		EventsDAO dao = new EventsDAOImpl(getServletContext());
+		Collection<Event> events = dao.getAll();
+		dao.remove(0);
+
+		response.sendRedirect("/admin/events");
+	}
+
 }
