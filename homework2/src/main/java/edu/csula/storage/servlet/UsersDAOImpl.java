9a35253b4ad16044f6666e86c5359c23dc0edf89
Nicolas Sirimongkol <nsirimo@calstatela.edu>
2018-04-20 21:03:00 -0700

    All user test pass

--- homework2/src/main/java/edu/csula/storage/servlet/UsersDAOImpl.java
+++ homework2/src/main/java/edu/csula/storage/servlet/UsersDAOImpl.java
@@ -1,12 +1,10 @@
 package edu.csula.storage.servlet;
 
-import java.util.Collection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
 
 import edu.csula.models.User;
 import edu.csula.storage.UsersDAO;
@@ -24,8 +22,6 @@ public class UsersDAOImpl implements UsersDAO {
 
 	@Override
 	public boolean authenticate(String username, String password) {
-		// TODO: check if username/password combination is valid and store the
-		//       username/password into the session
 		Object data = context.getAttribute(CONTEXT_NAME);
 		List<User> tempList;
 		if (data == null) {
@@ -44,21 +40,15 @@ public class UsersDAOImpl implements UsersDAO {
 
 	@Override
 	public Optional<User> getAuthenticatedUser() {
-		// TODO: return the authenticated user if there is any
-		Optional<User> userOp = Optional.empty();
 		User tempUser = (User) context.getAttribute(CONTEXT_NAME);
-		userOp = Optional.of(tempUser);
-
-		return userOp;
+		if (tempUser == null) {
+			return Optional.empty();
+		}
+		return Optional.of(tempUser);
 	}
 
 	@Override
 	public void logout() {
-		// TOOD: log user out using `invalidate`
-		HttpSession tempSession = (HttpSession) context.getSessionContext();
-		if (tempSession != null) {
-			tempSession.invalidate();
-		}
-
+		context.invalidate();
 	}
 }
